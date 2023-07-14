2091be7508fa73568f4d9cbaf7c10c71c3afef18
Nathan Clover <cloverww04@gmail.com>
2023-07-14 00:56:56 -0500

    ImplementingMainMenu

--- Plant.cs
+++ Plant.cs
@@ -1,9 +1,10 @@
-
+#pragma warning disable CA1050
 public class Plant {
     public string? Species { get; set; }
     public int LightNeeds { get; set; }
     public decimal AskingPrice { get; set; }
     public string? City { get; set; }
     public bool Sold { get; set; }
+    public string? Zip { get; set; }
 
 }
