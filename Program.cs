b30b98fabf78a2e46c7f4d62a3bc51643139c4da
Nathan Clover <cloverww04@gmail.com>
2023-07-13 19:15:13 -0500

    menu

--- Program.cs
+++ Program.cs
@@ -40,7 +40,44 @@ string greeting = @"Welcome to ExtraVert!
 We got plants if you got money.";
 Console.WriteLine(greeting);
 Console.WriteLine("\n");
-ViewPlantDetails();
+string choice = null!;
+while(choice != "0") {
+    Console.WriteLine(@"Choose from the following options:
+    0. Exit
+    1. Display all plants
+    2. Post a plant to be adopted
+    3. Adopt a plant
+    4. Delist a plant");
+    
+    choice = Console.ReadLine()!;
+
+    if (choice == "0") {
+        Console.WriteLine("Goodbye!");
+    }
+    else if ( choice == "1") {
+        Console.WriteLine("Display all plants");
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadKey();
+    }
+    else if ( choice == "2") {
+        Console.WriteLine("Post a plant to be adopted");
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadKey();
+    }
+    else if( choice == "3") {
+        Console.WriteLine("Adopt a plant");
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadKey();
+    }
+    else if(choice == "4") {
+        Console.WriteLine("Delist a plant");
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadKey();
+    }
+    else {
+        Console.WriteLine("\nPlease enter a number between 0 and 4\n");
+    }
+}
 
 
 
